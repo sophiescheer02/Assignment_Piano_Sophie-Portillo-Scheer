741edac9b18a0fccb110becee08af89b6c9fe141
sophiescheer02 <sophie.scheer@t-online.de>
2022-04-16 19:38:59 +0200

    First try to implement audios, Change key color

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
 
-import { helloWorld, Beispiel } from "./myModule";
-import { alertMe } from "./myOtherModule";
+//import { helloWorld, Beispiel } from "./myModule";
+//import { alertMe } from "./myOtherModule";
+
+let allkeys = ['c3', 'a3'];
+let ton;
+
+let c3 = document.querySelector('#c3');
+let a3 = document.querySelector('#a3');
+
+
+c3?.addEventListener('click', function () {
+  let ton = "c3";
+  playAudio(ton);
+  activateButton(ton);
+});
+
+a3?.addEventListener('click', function () {
+  let ton = "a3";
+  playAudio(ton);
+  activateButton(ton);
+});
+
+
+
 
 
