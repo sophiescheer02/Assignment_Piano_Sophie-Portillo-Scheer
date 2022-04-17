e204d65d2337a6e4b7bae26412c49d82744d1c63
sophiescheer02 <sophie.scheer@t-online.de>
2022-04-17 12:47:19 +0200

    Try to reduce code (DRY-Prinzip)

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,17 @@
 
 let allkeys = ['c3', 'a3'];
 let ton;
-
 let c3 = document.querySelector('#c3');
 let a3 = document.querySelector('#a3');
 
 
-c3?.addEventListener('click', function () {
+/* c3?.addEventListener('click', function () {
   const ton = "c3";
   playAudio(ton);
   activateButton(ton);
-});
+}); */
 
-key?.addEventListener('click', function () {
+a3?.addEventListener('click', function () {
   let ton = "a3";
   playAudio(ton);
   activateButton(ton);
