beb0c0e0c5918d03b29b0fd874cf89717d8fe4b8
sophiescheer02 <sophie.scheer@t-online.de>
2022-04-18 14:56:36 +0200

    Match Key ID to Audio on Keyboard

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,26 @@
 //import { helloWorld, Beispiel } from "./myModule";
 //import { alertMe } from "./myOtherModule";
 
-let c3 = document.querySelector('#c3');
-let a3 = document.querySelector('#a3');
+/*let c3 = document.querySelector('#c3');
+let a3 = document.querySelector('#a3');*/
+let linkKeyToTon: string[] = [];
 let keyw = document.querySelectorAll('.key.white');
 let keyb = document.querySelectorAll('.key.black');
-let ton = "init";
+/*let ton = "init";*/
 
+keyw?.forEach(key => {
+  let x = key.getAttribute("id")
+  let z = key.getAttribute("keyid")
+  linkKeyToTon.push(x as string, z as string);
+  console.log(linkKeyToTon);
+})
+
+keyb?.forEach(key => {
+  let x = key.getAttribute("id")
+  let z = key.getAttribute("keyid")
+  linkKeyToTon.push(x as string, z as string);
+  console.log(linkKeyToTon);
+})
 
 keyw?.forEach(key => {
   key.addEventListener("click", () => {
@@ -25,6 +39,15 @@ keyb?.forEach(key => {
     activateButton(key);
   });
 })
+
+
+document.addEventListener("keydown", e => {
+  const x = e.key;
+  //const ton = key.getAttribute("keyid");
+    //playAudio(ton as string);
+    //activateButton(key);
+  console.log(x);
+  });
 
 
 /* c3?.addEventListener('click', function () {
