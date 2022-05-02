85f9116b745ecd641a3067228fb75180fc73da68
sophiescheer02 <sophie.scheer@t-online.de>
2022-05-02 12:51:20 +0200

    Function to add CSS classes to curtain

--- src/curtain.ts
+++ src/curtain.ts
@@ -1,7 +1,15 @@
 
 export function curtainOpenWithEnter(e: any):void
 {
+    let curtainObject = document.getElementById("curtainObject") as HTMLElement;
+	
+	let show = document.getElementById("show") as HTMLElement;
+	
+	let openWithEnter = document.getElementById("openWithEnter") as HTMLElement;
+	
 	if(e.keyCode == 13) {
-		startShow();
+        curtainObject.className = "opencurtain";
+        show.className = "extend";
+        openWithEnter.className = "text";
 	}
 }
